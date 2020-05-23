8cc1e441a50c2a5d4c1ee6601a22a8caf14fd483
prerns <prerns@uw.edu>
2020-05-22 17:59:50 -0700

    completed chart 2

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -4,15 +4,18 @@ library("tidyr")
 
 emission_data <- read.csv("data/fossil-fuel-co2-emissions-by-nation.csv")
 
+#data for top 10 emitting countries in 2014 
 data_2014 <- emission_data %>%
   filter(Year == 2014) %>%
   arrange(-Total) %>%
   slice(1:10)
 
+#prepare the data to be able to place into a stacked chart
 gather_data <- data_2014 %>%
   select(Country, Solid.Fuel, Liquid.Fuel, Gas.Fuel, Cement, Gas.Flaring) %>%
   gather(key = carbon_emissions, value = type, -Country)
 
+#create a stacked bar chart based off emission type of top 10 emitting countries
 stacked_graph <- ggplot(data = gather_data) +
   geom_col(mapping = aes(x = Country, y = type, fill = carbon_emissions)) +
   xlab("Country") +
