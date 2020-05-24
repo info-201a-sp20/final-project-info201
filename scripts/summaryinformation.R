324c32a1272563ea7cc9729d1b4ac9d03fcc79b0
arnavkgupta <agupta8@uw.edu>
2020-05-24 02:05:09 -0700

    summary information script

---
+++ scripts/summaryinformation.R
@@ -0,0 +1,47 @@
+rm(list = ls())
+
+get_summary_info <- function(emission_data) {
+  country_with_most_emissions <- data %>%
+    group_by(Country) %>%
+    summarize(total_impact = sum(Total)) %>%
+    filter(total_impact == max(total_impact)) %>%
+    pull(Country)
+  
+  year_with_most_emissions <- data %>%
+    group_by(Year) %>%
+    summarize(total_impact = sum(Total)) %>%
+    filter(total_impact == max(total_impact)) %>%
+    pull(Year)
+  
+  most_emissions_per_capita <- data %>%
+    group_by(Country) %>%
+    summarize(total_capita = sum(Per.Capita)) %>%
+    filter(total_capita == max(total_capita)) %>%
+    pull(Country)
+  
+  year_most_emissions_per_capita <- data %>%
+    group_by(Year) %>%
+    summarize(total_capita = sum(Per.Capita)) %>%
+    filter(total_capita == max(total_capita)) %>%
+    pull(Year)
+  
+  country_with_least_emissions <- data %>%
+    group_by(Country) %>%
+    summarize(total_impact = sum(Total)) %>%
+    filter(total_impact == min(total_impact)) %>%
+    pull(Country)
+  
+  year_with_least_emissions <- data %>%
+    group_by(Year) %>%
+    summarize(total_impact = sum(Total)) %>%
+    filter(total_impact == min(total_impact)) %>%
+    pull(Year)
+  
+  list <- c(country_with_most_emissions, 
+            year_with_most_emissions, 
+            most_emissions_per_capita,
+            year_most_emissions_per_capita,
+            country_with_least_emissions,
+            year_with_least_emissions)
+  list
+}
