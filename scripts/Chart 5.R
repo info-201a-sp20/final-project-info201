7320144fe12a280b348180cdd33ca8bb4702a286
dattadave8 <dattad8@uw.edu>
2020-05-24 21:31:05 -0700

    finished chart 5

---
+++ scripts/Chart 5.R
@@ -0,0 +1,21 @@
+emission_data <- read.csv("data/fossil-fuel-co2-emissions-by-nation.csv")
+library("dplyr")
+library("ggplot2")
+
+ bar_data <- function (emission_data) {
+   liquid_fuel <- emission_data %>% 
+     group_by(Country) %>%
+     summarize(total_fuel = sum(Solid.Fuel)) %>%
+     top_n(10)
+ 
+ bar_chart <- ggplot(liquid_fuel) + 
+   geom_bar(mapping = aes(x = Country, fill = Solid.Fuel), color = "dark blue") + 
+   labs(
+     title = "Total Fossil Fuels CO Emissions By Solid Fuels",
+     X = "Country",
+     Y = "Total Solid Fuel",
+     legend = FALSE 
+   )
+ return(bar_chart)
+ }
+ 
