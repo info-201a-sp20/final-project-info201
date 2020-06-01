b4b22abca91fb0a4d94f605b1a3c502316a4d20d
prerns <prerns@uw.edu>
2020-06-01 16:42:41 -0700

    page 3 ui combined

---
+++ page3.R
@@ -0,0 +1,29 @@
+side_panel <- sidebarPanel(
+  sliderInput(
+    inputId = "year_graph",
+    label = h5("select the desired year"),
+    min = 1751,
+    max = 2014,
+    value = c(1900),
+    step = 1,
+  ),
+  textOutput(outputId = "message")
+)
+
+main_panel <- mainPanel(
+  plotlyOutput(outputId = "stackedplotly")
+)
+
+
+page_three <- tabPanel(
+  "Breakdown Emissions",
+  p(
+    "The purpose of this chart is to look at a breakdown of each carbon emitting
+    source in relation to the total for the top 10 carbon emitting countries in the
+    year of your choosing (1751-2014)"
+  ),
+  sidebarLayout(
+    side_panel,
+    main_panel
+  )
+)
