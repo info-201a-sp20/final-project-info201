d1554c3d4919c5b1a183a3b514482cebf4a42ad0
dattadave8 <dattad8@uw.edu>
2020-06-01 20:51:20 -0700

    added more

--- page5i.R
+++ page5i.R
@@ -1,21 +1,55 @@
-sidepanel <- sidebarPanel(
-  selectInput(
-    inputId = "country_choice",
-    choices = c("UNITED STATES", "CHINA(MAINLAND)", "USSR", "UNITED KINGDOM", "JAPAN", "GERMANY", "INDIA", "RUSSIAN FEDERATION",
-                "FRANCE(INCLUDING MONACO)", "CANADA"),
-    label = h3("Choose A Country")
-  ),
-  textOutput(outputId = "summary_info_per_country")
-)
-mainPanel <- mainPanel(
-  plotlyOutput(outputId = "summary_info_per_country")
-)
-
-page5 <- tabPanel(
-  "Top Ten Countries With The Highest Emissions",
-  headerPanel("Top Ten Countries With The Highest Emissions"),
-  sidebarLayout(
-    side_panel,
-    main_panel
+sidepanel <- sidebarLayout(
+  selectPanel(
+    p("As seen through the various charts in the proceeding tabs, fossil fuel carbon emissions have been 
+      a rising and stagnant issue for years now. Starting from the effects of the industrial revolution, 
+      the effects of fossil fuel emissions have worsen and increased throughout the years. 
+      Countries such as the United States of America, China(mainland), USSR, United Kingdom, Japan, Germany, India, 
+      Russian Federation, France (including Monaco), and Canada have been evaluated to be the highest emitting countries in the world. 
+      But despite these countries, the overall trend of the carbon emissions in the world has been rising over the last 75 years. 
+      There has been a rise of %% and the numbers are projected to continue rising."), 
+    img("",
+         src = paste0(
+             "Top Ten Countries With Highest Emissions",
+           "/image/Number1"
+         ),
+         height = "100%", width = "100%"
+    ), 
+    img("",
+        src = paste0(
+          "Total Fossil Fuels Over Last 50 Years",
+          "/image/Number2"
+        ),
+        height = "100%", width = "100%"
+    ),
+     p("Furthermore, through our analysis it has been apparent that the biggest contributing fuel to carbon emissions 
+       in the top ten emitting countries in 2014 has been solid fuel. This being the most up to date information about the
+       fuel that contributes the most to carbon emissions confirms that to change these patterns this is the fuel that needs 
+       to be targeted as it is seen through the most recent data. Of these top ten countries; China, the United States, 
+       and India tend to be the highest at using solid fuels to emit carbon into the atmosphere. This could be due to
+       the fact that both India, China, and US are highly populated countries that need a large supply of fuel to meet the need, 
+       therefore they have resided to the most easy to access and least expensive source of fossil fuel. Of these top ten 
+       countries emitting the most carbon, the top five countries whose has the highest rate of total change in fossil 
+       fuel use continues to be China and the United States. This essentially indicates that these two countries emit 
+       the highest carbon without changing for years, whereas India has changed throughout the years. These statistics 
+       can be visually understood through the charts below. "), 
+    img("",
+        src = paste0(
+          "Top Ten Countries With Highest Emissions in 2014",
+          "/image/Number3"
+        ),
+        height = "100%", width = "100%"
+    ), 
+    img("",
+        src = paste0(
+          "Total Five Countries With A Change In Fossil Fuels",
+          "/image/Number4"
+        ),
+        height = "100%", width = "100%"
+    )
   )
 )
+
+
+
+
+  
