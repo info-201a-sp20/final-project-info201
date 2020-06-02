f691e85650f5214f5d41b9b46b199198b7ca38f8
pranavvasan <pvasan@uw.edu>
2020-06-01 17:44:44 -0700

    finished write-up for page2.R

--- page2.R
+++ page2.R
@@ -1,8 +1,8 @@
 emission_data <- read.csv("data/fossil-fuel-co2-emissions-by-nation.csv")
 
 page2 <- tabPanel(
-  "Total Emission Over Last 50 Years",
-  titlePanel("Total Emission Over Last 50 Years"),
+  "Fossil Fuel Emission Over Last 50 Years",
+  titlePanel("Fossil Fuel Emission Over Last 50 Years"),
 
   sidebarLayout(
     sidebarPanel(
@@ -15,7 +15,25 @@ page2 <- tabPanel(
     ),
 
     mainPanel(
-      plotlyOutput(outputId = "line_graph")
+      plotlyOutput(outputId = "line_graph"),
+
+      h3("Chart Description"),
+      p("This chart displays information about fossil fuel emission",
+        "over the last 50 years (1964-2014). It shows the trends for",
+        "each of the individual fuel types (solid, liquid, and gas)",
+        "as well as for the total fossil fuel emissions. This demonstrates",
+        "how emissions may have increased, specifically over the last 50",
+        "years, as well as which types of fuel had the greatest impact.",
+        "Overall, the chart answers questions regarding how climate",
+        "change has been impacted in recent times by quantifying the idea",
+        "through specific fossil fuel emission data visualizations and",
+        "widgets. It displays which types of fuel had the greatest impact",
+        "and which types had the least impact over the last 50 years.",
+        "These results can be used to take the necessary measures to reduce",
+        "emissions of the fuel types that have been impacting the environment",
+        "most and, ultimately, the effect of climate change."
+      )
+
     )
   )
 )
